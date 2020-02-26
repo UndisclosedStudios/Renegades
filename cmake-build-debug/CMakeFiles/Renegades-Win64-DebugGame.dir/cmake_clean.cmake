8e14de4da69fcce27812cd5f1f015f11428a7e74
infrandomness <infrandomness@gmail.com>
2020-02-26 05:10:41 +0100

    Untrack and ignore useless folders

--- cmake-build-debug/CMakeFiles/Renegades-Win64-DebugGame.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Renegades-Win64-DebugGame"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Renegades-Win64-DebugGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/Renegades.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Renegades"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Renegades.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
